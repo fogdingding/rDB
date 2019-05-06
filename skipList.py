e70cbfded547c37538800a44a9c9a49833c13db8
fogdingding <fogdingding@gmail.com>
2019-05-07 00:30:38 +0800

    for version1.2.

--- skipList.py
+++ skipList.py
@@ -2,7 +2,8 @@ from random import randint, seed
 
 class SkipNode:
     """A node from a skip list"""    
-    def __init__(self, height = 0, elem = None):
+    def __init__(self, height = 0, elem = None , key=None):
+        self.key = key
         self.elem = elem
         self.next = [None]*height
 
@@ -27,10 +28,10 @@ class SkipList:
     
     def contains(self, elem, update = None):
         return self.find(elem, update) != None
-
+    
     def randomHeight(self):
         height = 1
-        while randint(1, 2) != 1:
+        while randint(1, 2)!= 1:
             height += 1
         return height
 
@@ -43,9 +44,9 @@ class SkipList:
             update[i] = x
         return update
         
-    def insert(self, elem):
+    def insert(self, elem, key):
 
-        node = SkipNode(self.randomHeight(), elem)
+        node = SkipNode(self.randomHeight(), elem, key)
 
         self.maxHeight = max(self.maxHeight, len(node.next))
         while len(self.head.next) < len(node.next):
@@ -73,6 +74,39 @@ class SkipList:
         for i in range(len(self.head.next)-1, -1, -1):
             x = self.head
             while x.next[i] != None:
-                print (x.next[i].elem,end="")
+                print (x.next[i].elem + '-',end="")
                 x = x.next[i]
-            print ('-')
+            print ('')
+    
+# 
+    def find2(self, elem, update = None):
+        if update == None:
+            update = self.updateList2(elem)
+        if len(update) > 0:
+            candidate2 = update[0].next[0]
+            if candidate2 != None and candidate2.key == elem:
+                return candidate2
+        return None
+    
+    def contains2(self, elem, update = None):
+        return self.find(elem, update) != None
+
+
+    def updateList2(self, elem):
+        update = [None]*self.maxHeight
+        x = self.head
+        for i in reversed(range(self.maxHeight)):
+            while x.next[i] != None and x.next[i].key < elem:
+                x = x.next[i]
+            update[i] = x
+        return update
+    
+
+
+url_list = ["https://www.youtube.com/watch?v=1R2vIsIPI38","https://www.youtube.com/watch?v=1R2vIsIPI38","https://www.youtube.com/watch?v=adex5YdEbOs","https://www.youtube.com/watch?v=vlCumuwLkEM","https://www.youtube.com/watch?v=d25EQaECNnU","https://www.youtube.com/watch?v=Qh8DoWbumgs&t=4s","https://www.youtube.com/watch?v=OE9mcx_iJrE"]
+sSkipList = SkipList()
+for index,url in enumerate (url_list):
+    key = "{}".format(index)
+    elem = url
+    sSkipList.insert(elem,key)
+sSkipList.printList()
